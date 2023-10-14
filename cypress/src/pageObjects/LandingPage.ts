b2a651c8f56b953ff989bb62b8a6fc06bbef7ac9
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2023-10-14 18:43:37 +1000

    get a model for class working and default parameter assignment working

---
+++ cypress/src/pageObjects/LandingPage.ts
@@ -0,0 +1,44 @@
+type Options = {
+    baseUrl?: string;
+    verifyPage?: boolean
+} 
+
+class LandingPage {
+    baseUrl: string;
+
+    visit({baseUrl = 'https://magento.softwaretestingboard.com/', verifyPage = false}: Options) {
+
+        cy.visit(baseUrl);
+
+        if(verifyPage) {
+            cy.get('header').should((header) => {
+                let searchBarValue = header.val;
+                let miniCart = header.find('.minicart-wrapper');
+                let signInEl = header.find('.authorization-link');
+                let createAccountEl = header.find('li');
+
+                expect(searchBarValue).to.contain(/Search entire store here/i);                
+                expect(miniCart).to.be.visible;
+                expect(signInEl).to.be.visible;
+                expect(signInEl.text()).to.contain("Sign In");
+                expect(createAccountEl).to.be.visible;
+                expect(createAccountEl.text()).to.contain("Create an Account");
+            });
+
+            cy.get('#store\\.menu').should((items) => {
+                let namesList = ["What's New", "Women", "Men", "Gear", "Training", "Sale"];
+                
+                for(let i = 0; i < namesList.length; i++) {
+                    expect(items).to.contain(namesList[i]);
+                }
+
+                
+            })
+
+        }
+        
+    }
+
+}
+
+export default new LandingPage();
